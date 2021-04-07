9b89df10544d03ceece6b9aba70e6e906d959426
Alexander <alexandr-altukhov2013@yandex.ru>
2021-04-07 16:46:02 +0300

    feat: frozen table

--- lib/main.dart
+++ lib/main.dart
@@ -60,8 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
   static List<DataRow> rows = [
     DataRow(selected: selected, cells: [
       DataCell(Text("Математика")),
-      DataCell(Text("+")),
-      DataCell(Text("-")),
+      DataCell(Icon(Icons.add)),
+      DataCell(Icon(Icons.remove)),
       DataCell(Text("-")),
       DataCell(Text("+")),
       DataCell(Text("+")),
@@ -113,23 +113,126 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: dataTable,
-          ),
+        //body: SingleChildScrollView(
+        //  scrollDirection: Axis.vertical,
+        //  child: SingleChildScrollView(
+        //    scrollDirection: Axis.horizontal,
+        //    child: dataTable,
+        //  ),
+        //),
+        body: getTableWidget(),
+        bottomNavigationBar: ConvexAppBar(
+          style: TabStyle.react,
+          backgroundColor: Colors.blue,
+          items: [
+            TabItem(icon: Icons.list_alt_outlined),
+            TabItem(icon: Icons.check_box_outline_blank_sharp),
+            TabItem(icon: Icons.check_box_outlined),
+          ],
+          initialActiveIndex: 0 /*optional*/,
+          onTap: (int i) => print('click index=$i'),
         ),
-      bottomNavigationBar: ConvexAppBar(
-        style: TabStyle.react,
-        backgroundColor: Colors.blue,
-        items: [
-          TabItem(icon: Icons.list_alt_outlined),
-          TabItem(icon: Icons.check_box_outline_blank_sharp),
-          TabItem(icon: Icons.check_box_outlined),
-        ],
-        initialActiveIndex: 1 /*optional*/,
-        onTap: (int i) => print('click index=$i'),
-      ),);
+    );
   }
+
+  Widget getTableWidget(){
+
+    return Container(
+      child: HorizontalDataTable(
+        leftHandSideColumnWidth: 100,
+        rightHandSideColumnWidth: 600,
+        isFixedHeader: true,
+        headerWidgets: _getTitlesWidget(),
+        leftSideItemBuilder: _generateFirstColumnRow,
+        rightSideItemBuilder: _generateRightHandSideColumnRow,
+        itemCount: 4,
+        rowSeparatorWidget: const Divider(
+          color: Colors.black54,
+          height: 1.0,
+          thickness: 0.0,
+        ),
+      ),
+      height: MediaQuery.of(context).size.height,
+    );
+  }
+
+  List<Widget> _getTitlesWidget(){
+    //TODO get title list from server 
+    List<Widget> res = [];
+    List<String> titles = ["Предмет", "Lab1", "Lab2", "Lab3", "Lab1", "Lab2", "Lab3"];
+    for (String v in titles) {
+      res.add(Container(
+        child: Text(v, style: TextStyle(fontWeight: FontWeight.bold)),
+        width: 100,
+        height: 56,
+        padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+        alignment: Alignment.centerLeft,
+      ));
+    }
+    return res;
+  }
+
+  Widget _generateFirstColumnRow(BuildContext context, int index) {
+    //TODO get list from server 
+    print("index in first: $index");
+    List<String> items = ["Математика", "Русский", "Информатика", "Физика"];
+    return Container(
+      child: Text(items[index]),
+      width: 100,
+      height: 52,
+      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+      alignment: Alignment.centerLeft,
+    );
+  }
+
+  Widget _generateRightHandSideColumnRow(BuildContext context, int index) {
+    print("index in right: $index");
+    return Row(
+      children: <Widget>[
+        Container(
+          child: Text('+'),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text('-'),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text('+'),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text('+'),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text('-'),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+        Container(
+          child: Text('+'),
+          width: 100,
+          height: 52,
+          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+        ),
+      ],
+    );
+  }
+
 }
