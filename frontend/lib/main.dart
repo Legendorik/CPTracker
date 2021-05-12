cbe1e2f523371b68b74dc1e824e0ffa2ab8ea694
Alexander <alexandr-altukhov2013@yandex.ru>
2021-05-12 21:18:46 +0300

    feat: print more error info

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -5,11 +8,26 @@ import 'package:flutter/services.dart';
 import 'my_home_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]).then((_) => runApp(MyApp()));
+
+
+
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    //await myErrorsHandler.initialize();
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.dumpErrorToConsole(details);
+      //myErrorsHandler.onError(details);
+      //exit(1);
+    };
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]).then((_) => runApp(MyApp()));
+  }, (Object error, StackTrace stack) {
+    //myErrorsHandler.onError(error, stack);
+    //exit(1);
+  });
+  
 }
 
 class MyApp extends StatelessWidget {
